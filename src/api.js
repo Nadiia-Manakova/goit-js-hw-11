2e0aeee5324286413114d4254b9544491feffc43
Nadiia-Manakova <nadiiamanakova33@gmail.com>
2023-11-04 22:58:28 +0100

    finished

--- src/api.js
+++ src/api.js
@@ -2,13 +2,21 @@ import axios from 'axios'
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-export const getData = () => {
-    const urlSeaParams = new URLSearchParams({
-        key: '36867623-ba51f98b382c0f13b01ef8d79',
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        q: '',
-    })
-    return axios.get(`?${urlSeaParams}`).then(res => res.data)
-}
+export const getData = async (searchQuery, page = 1) => {
+    try {
+        const urlSeaParams = new URLSearchParams({
+            key: '36867623-ba51f98b382c0f13b01ef8d79',
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+            q: searchQuery, 
+            page,
+            per_page: 40,
+        });
+
+        const response = await axios.get(`?${urlSeaParams}`);
+        return response.data; 
+    } catch (error) {
+        return error
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,28 +1,43 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import {getData} from './api'
+import { getData } from './api'
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 const formEl = document.querySelector('.search-form');
 const galleryEl = document.querySelector('.gallery');
+const loadMoreEl = document.querySelector('.load-more');
 
+let page = 1;
+let value = '';
 formEl.addEventListener('submit', onSubmit);
 
 async function onSubmit(evt) {
     evt.preventDefault();
-    const value = evt.target.elements.searchQuery.value.trim();
+    value = evt.target.elements.searchQuery.value.trim();
     if (!value) return;
-    
-    console.log(value)
+    page = 1;
+    //console.log(value)
     try {
-        const { totalHits, hits } = await getData(value);
+        const { totalHits, hits } = await getData(value, page);
+        
+        //console.log(hits.length)
         if (!hits.length) {
-           return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+            return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
         }
-
+        Notify.info(`Hooray! We found ${totalHits} images.`);
+        clearGallery();
+        if (totalHits <= 40) {
+            loadMoreEl.classList.add(`is-hidden`);
+        } else {
+            loadMoreEl.classList.remove(`is-hidden`);
+        }
         const markup = createGallery(hits);
         addMarkup(markup)
     } catch (error) {
         console.log(error)
+    } finally {
+        evt.target.reset();
     }
     
 }
@@ -54,4 +69,27 @@ function createGallery(items = []) {
 
 function addMarkup(markup = '') {
     galleryEl.insertAdjacentHTML('beforeend', markup)
+}
+
+
+function clearGallery() {
+    galleryEl.innerHTML = ''; 
+}
+
+loadMoreEl.addEventListener('click', onClick)
+
+async function onClick() {
+    try {
+        page += 1;
+        const { totalHits, hits } = await getData(value, page);
+        const markup = createGallery(hits);
+        addMarkup(markup)
+
+        if (page * 40 >= totalHits) {
+            loadMoreEl.classList.add('is-hidden');
+            Notify.info(`We're sorry, but you've reached the end of search results.`);
+        }
+    } catch (error) {
+        console.log(error)
+    }
 }
